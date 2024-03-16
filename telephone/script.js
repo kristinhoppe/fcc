b10091e7455e195a5a56d5e348f7f30694d06f72
kristinhoppe <59065804+kristinhoppe@users.noreply.github.com>
2024-03-15 20:16:48 -0700

    refactor

--- telephone/script.js
+++ telephone/script.js
@@ -4,6 +4,20 @@ const userInput = document.getElementById("user-input");
 const validateBtn = document.getElementById("check-btn");
 const results = document.getElementById("results-div");
 let checkNum = [];
+
+const tenDigit = /^\d{10}/g;
+const elevenDigit = /1(\d{10})/g;
+const twelveDigit = /^(\(|-|\s)?(\d{3})(\)|\s|-)?(\d{3})(\s|-)?(\d{4})$/g;
+const thirteenDigit = /^1(\()(\d{3})(\))(\d{3})(\d{4})$/g;
+const fourteenDigit = /^1(\(|\s|-)?(\d{3})(\)|-|\s|)?(\d{3})(-|\s)?(\d{4})$/g;
+const denyList = [
+  tenDigit,
+  elevenDigit,
+  twelveDigit,
+  thirteenDigit,
+  fourteenDigit,
+];
+
 //listen to the page
 //.addEventListener("click")
 //.addEventListener("enter")
@@ -21,68 +35,40 @@ userInput.addEventListener("keydown", (e) => {
 
 //clear text
 function clearForm() {
-  results.style.display = "hidden";
+  //results.style.display = "hidden";
   document.getElementById("user-input").value = "";
 
   checkNum = [];
 }
 
 function getUserInput(userInput) {
-  checkNum = "";
   if (userInput == "") {
     results.innerText = alert("Please enter a valid number");
     clearForm();
   } else {
-    checkNum = userInput.replace(/\s/g, "");
-    console.log(regexCheck(checkNum));
-
-    if (!results.innerText) {
-      results.innerText = regexCheck
-        ? "Invalid US Number: " + checkNum
-        : "Valid US Number: " + checkNum;
+    checkNum = userInput;
+    console.log(userInput);
+    console.log(checkNum);
+    const isBad = (userInput) =>
+      denyList.some((regex) => regex.test(userInput));
+    if (userInput.length === 10) {
+      (userInput) => regex.text(userInput);
     }
+    results.textContent = !isBad(userInput)
+      ? "Invalid US number: " + userInput
+      : "Valid US number: " + userInput;
+    userInput.value = "";
     results.classList.remove("hidden");
   }
 }
 
-function regexCheck(checkNum) {
-  const tenDigit = /^\d{10}/g;
-  const elevenDigit = /1\d{10}/g;
-  const twelveDigit = /^(\(|-|\s)?(\d{3})(\)|\s|-)?(\d{3})(\s|-)?(\d{4})$/g;
-  const thirteenDigit = /^1(\()(\d{3})(\))(\d{3})(\d{4})$/g;
-  const fourteenDigit = /^1(\(|\s|-)?(\d{3})(\)|-|\s|)?(\d{3})(-|\s)?(\d{4})$/g;
-  const denyList = [
-    tenDigit,
-    elevenDigit,
-    twelveDigit,
-    thirteenDigit,
-    fourteenDigit,
-  ];
+//regex check for
+// 10 or 11 digits
+//then if 10
+// optional parenthesis around digits 2-4
+//optional dashes
+//optional spaces
+// if 11 digits long 1st digit must be 1
 
-  const isBad = (checkNum) => denyList.some((regex) => regex.test(checkNum));
-
-  switch (checkNum) {
-  }
-
-  if (checkNum.length < 10 || checkNum.length > 14) {
-    console.log("False: " + checkNum.length + checkNum);
-    return false;
-  } else {
-    if (checkNum.length === 11) {
-      console.log("True: " + checkNum.length);
-    } else {
-      console.log("True: " + checkNum.length);
-    }
-
-    //regex check for
-    // 10 or 11 digits
-    //then if 10
-    // optional parenthesis around digits 2-4
-    //optional dashes
-    //optional spaces
-    // if 11 digits long 1st digit must be 1
-    return true;
-  }
-  //strip dashes/spaces
-  //check for parenthesis location
-}
+//strip dashes/spaces
+//check for parenthesis location
