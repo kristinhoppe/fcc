476c80cb2c9f91859857d57f44804dd64c43ddcc
kristinhoppe <59065804+kristinhoppe@users.noreply.github.com>
2024-03-19 10:59:31 -0700

    mismatched parens not working

--- telephone/script.js
+++ telephone/script.js
@@ -1,22 +1,15 @@
-/** @format */
 
 const userInput = document.getElementById("user-input");
 const validateBtn = document.getElementById("check-btn");
 const results = document.getElementById("results-div");
-let checkNum = [];
 
-const tenDigit = /^\d{10}/g;
-const elevenDigit = /1(\d{10})/g;
-const twelveDigit = /^(\(|-|\s)?(\d{3})(\)|\s|-)?(\d{3})(\s|-)?(\d{4})$/g;
-const thirteenDigit = /^1(\()(\d{3})(\))(\d{3})(\d{4})$/g;
-const fourteenDigit = /^1(\(|\s|-)?(\d{3})(\)|-|\s|)?(\d{3})(-|\s)?(\d{4})$/g;
-const denyList = [
-  tenDigit,
-  elevenDigit,
-  twelveDigit,
-  thirteenDigit,
-  fourteenDigit,
-];
+// const tenDigit = /^\d{10}/g;
+//const elevenDigit = /1(\d{10})/;
+const twelveDigit = /^(\(|-|\s)?(\d{3})(\)|\s|-)?(\d{3})(\s|-)?(\d{4})$/;
+const thirteenDigit = /^1(\(|\s|-)(\d{3})(\)|\s|-)(\d{3})(\s|-)(\d{4})/;
+const fourteenDigit = /^1\s?(\(|\s|-)(\d{3})(-|\s|\))\s?(\d{3})(-|\s|\))(\d{4})/;
+const denyList = [ twelveDigit, thirteenDigit, fourteenDigit];
+const parenCheck = /\(|\)/g
 
 //listen to the page
 //.addEventListener("click")
@@ -36,39 +29,38 @@ userInput.addEventListener("keydown", (e) => {
 //clear text
 function clearForm() {
   //results.style.display = "hidden";
-  document.getElementById("user-input").value = "";
 
-  checkNum = [];
+  results.textContent = "";
+  return;
 }
 
+function matchCount(userInput){
+  const check =  [...userInput.matchAll(parenCheck)]; 
+  const isMatched = (check.length%2 === 0) ? true : false
+  return isMatched
+
+}
+
+
 function getUserInput(userInput) {
+  
   if (userInput == "") {
-    results.innerText = alert("Please enter a valid number");
-    clearForm();
-  } else {
-    checkNum = userInput;
-    console.log(userInput);
-    console.log(checkNum);
-    const isBad = (userInput) =>
+    results.innerText = alert("Please provide a phone number");
+    return
+  } else if (userInput.length < 10 || ![...userInput.matchAll(parenCheck)].length%2 === 0)
+  {
+    results.textContent = "Invalid US number: " + userInput
+    userInput.value = "";
+    results.classList.remove("hidden")
+    return
+  }
+  else {
+    const isGood = (userInput) =>
       denyList.some((regex) => regex.test(userInput));
-    if (userInput.length === 10) {
-      (userInput) => regex.text(userInput);
-    }
-    results.textContent = !isBad(userInput)
-      ? "Invalid US number: " + userInput
-      : "Valid US number: " + userInput;
+    results.textContent = isGood(userInput)
+      ? "Valid US number: " + userInput
+      : "Invalid US number: " + userInput;
     userInput.value = "";
     results.classList.remove("hidden");
   }
 }
-
-//regex check for
-// 10 or 11 digits
-//then if 10
-// optional parenthesis around digits 2-4
-//optional dashes
-//optional spaces
-// if 11 digits long 1st digit must be 1
-
-//strip dashes/spaces
-//check for parenthesis location
